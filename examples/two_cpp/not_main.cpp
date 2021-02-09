86233e208fc9124752560961ea0842f5a4351b36
Andrew Loebs <andrew.loebs@gmail.com>
2021-02-08 20:58:46 -0500

    Added basic compile + linking.

---
+++ examples/two_cpp/not_main.cpp
@@ -0,0 +1,4 @@
+int calculate_something(int a, int b)
+{
+    return a ^ b;
+}
--- halfling/halfling.py
+++ halfling/halfling.py
@@ -1,7 +1,10 @@
 import toml
 from dataclasses import dataclass
+import subprocess
+from pathlib import Path, PurePath
 
-FILEPATH = "./halfling.toml"
+FILEPATH = "halfling.toml"
+OBJ_DIR = "obj"
 
 
 @dataclass
@@ -9,8 +12,21 @@ class Config:
     project_name: str
     compiler: str
     sources: list
+    build_dir: str = "build"
 
 
 if __name__ == "__main__":
+    # load config
     config = Config(**toml.load(FILEPATH))
-    print(config)
+    # create build + obj directory if they don't exist
+    obj_dir = Path(config.build_dir, OBJ_DIR)
+    obj_dir.mkdir(parents=True, exist_ok=True)
+    # compile object files
+    obj_files = []
+    for source in config.sources:
+        obj_file = (obj_dir / source).with_suffix(".o")
+        subprocess.run([config.compiler, "-o", obj_file, "-c", source])
+        obj_files.append(obj_file)
+    # link
+    subprocess.run([config.compiler] + obj_files +
+                   ["-o", PurePath(config.build_dir, config.project_name)])
